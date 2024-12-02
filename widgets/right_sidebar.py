b86c111ec2fd43324d19ccee78b01233866aa8a8
Ryan G <exogenesis@protonmail.com>
2024-12-02 16:20:41 +1100

    rename widgets

--- widgets/right_sidebar.py
+++ widgets/right_sidebar.py
@@ -7,16 +7,16 @@ from models.note import Note
 
 class ForwardLinksWidget(QListWidget):
     note_selected = Signal(int)  # Emitted when a note is selected, passes note_id
-    
+
     def __init__(self, parent=None):
         super().__init__(parent)
         self.itemDoubleClicked.connect(self._on_item_double_clicked)
-        
+
     def _on_item_double_clicked(self, item: QListWidgetItem):
         note_id = item.data(Qt.ItemDataRole.UserRole)
         if note_id:
             self.note_selected.emit(note_id)
-            
+
     def update_links(self, forward_links: List[Note]) -> None:
         """Update the list with new forward links"""
         self.clear()
@@ -25,7 +25,7 @@ class ForwardLinksWidget(QListWidget):
             item.setFlags(item.flags() & ~Qt.ItemFlag.ItemIsEnabled)  # Make non-clickable
             self.addItem(item)
             return
-            
+
         for note in forward_links:
             item = QListWidgetItem(note.title)
             item.setData(Qt.ItemDataRole.UserRole, note.id)
@@ -35,9 +35,9 @@ class ForwardLinksWidget(QListWidget):
 class RightSidebar(QSplitter):
     def __init__(self, handle_size=20, parent=None):
         super().__init__(Qt.Orientation.Vertical, parent)
-        self.additional_tree = NotesTreeWidget()
+        self.backlinks = NotesTreeWidget()
         self.forward_links = ForwardLinksWidget()
-        self.text_mid = QTextEdit()
+        self.tags = QTextEdit()
         self.text_bottom = QTextEdit()
 
         self._setup_ui(handle_size)
@@ -46,21 +46,21 @@ class RightSidebar(QSplitter):
         self.setHandleWidth(handle_size)
 
         # First add all widgets to the splitter
-        self.addWidget(self.additional_tree)
+        self.addWidget(self.backlinks)
         self.addWidget(self.forward_links)
-        self.addWidget(self.text_mid)
+        self.addWidget(self.tags)
         self.addWidget(self.text_bottom)
 
         # Then set up their properties
-        self.additional_tree.setHeaderLabel("Backlinks")
-        self.additional_tree.setMinimumWidth(200)
+        self.backlinks.setHeaderLabel("Backlinks")
+        self.backlinks.setMinimumWidth(200)
 
         self.forward_links.setMinimumHeight(100)
-        self.text_mid.setPlaceholderText("Tags")
+        self.tags.setPlaceholderText("Tags")
         self.text_bottom.setPlaceholderText(
             "Similar Pages (Not Yet Implemented, Don't Touch)"
         )
-        
+
     def update_forward_links(self, forward_links: List[Note]) -> None:
         """Update the forward links list with linked notes"""
         self.forward_links.update_links(forward_links)
