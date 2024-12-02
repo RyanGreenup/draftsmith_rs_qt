56b5d242b0b990e04d1e3f3dcb415e884376fd02
Ryan G <exogenesis@protonmail.com>
2024-12-02 12:36:10 +1100

    Allow C-c

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from PySide6.QtWidgets import QApplication
+import signal
 from PySide6.QtCore import QFile, QTextStream
 import sys
 from ui.menu_manager import create_file_menu
@@ -16,6 +17,9 @@ from widgets.main_window import NoteApp
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = NoteApp()
+
+     # Allow C-c to kill app
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
 
     # Load and apply the stylesheet
     with open("ui/styles/style.qss", "r") as file:
